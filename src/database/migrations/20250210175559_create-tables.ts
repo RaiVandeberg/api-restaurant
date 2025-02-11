163b39cf8cbee8e0dd17fb6d630976bb359e9e48
Raí <devvandenberg>
2025-02-11 19:25:03 -0300

    pedidos

--- src/database/migrations/20250210175559_create-tables.ts
+++ src/database/migrations/20250210175559_create-tables.ts
@@ -7,7 +7,7 @@ export async function up(knex: Knex): Promise<void> {
         table.increments("id").primary(),
         table.integer("table_number").notNullable(),
         table.timestamp("created_at").defaultTo(knex.fn.now()),
-        table.timestamp("apdated_at").defaultTo(knex.fn.now())
+        table.timestamp("updated_at").defaultTo(knex.fn.now())
     })
 }
 
